0fd821055c9822be56cfc9d39e707e48cd44ea59
Bryan-E-Flores-M <bryanfloresmejia7@gmail.com>
2026-04-15 21:06:57 -0400

    agregar dependencias mappetypes y swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder , SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+
+  const confg = new DocumentBuilder()
+    .setTitle('Api_de_Auto_Sale')
+    .setDescription('Api de Auto Sale')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, confg);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
