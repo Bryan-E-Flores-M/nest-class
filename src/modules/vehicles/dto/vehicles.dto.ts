3a2a5713f55f7a8c42b5f825e47035d2192dfb06
Bryan-E-Flores-M <bryanfloresmejia7@gmail.com>
2026-04-29 20:43:29 -0400

    agregando controllers dto y conectando con postman 2204

--- src/modules/vehicles/dto/vehicles.dto.ts
+++ src/modules/vehicles/dto/vehicles.dto.ts
@@ -1,1 +1,50 @@
-export class VehiclesDto {}
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import {
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateVehicleDto {
+  @IsNumber()
+  @IsOptional()
+  @ApiProperty()
+  id?: number;
+
+  @IsNumber()
+  @ApiProperty()
+  model_id!: number;
+
+  @IsString()
+  @MinLength(3)
+  @ApiProperty()
+  vin!: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  @ApiProperty()
+  year?: number;
+
+  @IsString()
+  @MinLength(3)
+  @ApiProperty()
+  color!: string;
+
+  @IsNumber()
+  @ApiProperty()
+  mileage!: number;
+
+  @IsNumber()
+  @ApiProperty()
+  price!: number;
+
+  @IsString()
+  @ApiProperty()
+  status!: string;
+}
+
+export class UpdateVehicleDto extends PartialType(CreateVehicleDto) {}
--- src/modules/vehicles/services/vehicles.service.ts
+++ src/modules/vehicles/services/vehicles.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { CreateVehicleDto } from '../dto/vehicles.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Vehicle } from '../entities/vehicle.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class VehiclesService {}
+export class VehiclesService {
+  constructor(
+    @InjectRepository(Vehicle)
+    private readonly vehicleRepository: Repository<Vehicle>,
+  ) {}
+
+  async create(createVehicleDto: CreateVehicleDto) {
+    console.log('Datos listos para guardar:', createVehicleDto);
+
+    const newVehicle = this.vehicleRepository.create(createVehicleDto);
+
+    await this.vehicleRepository.save(newVehicle);
+
+    return newVehicle;
+  }
+
+  findAll() {
+    return 'Lista de vehiculos';
+  }
+}
